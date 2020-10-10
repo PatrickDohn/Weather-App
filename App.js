60b34382f732df687573fc43b21daabc09e6c927
PatrickDohn <dohnpatrick1993@gmail.com>
2020-10-09 21:23:17 -0400

    updates to unitPicker and creates ReloadIcon

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 import * as Location from 'expo-location'
 import { AppLoading } from 'expo';
 import WeatherInfo from './components/WeatherInfo'
+import UnitsPicker from './components/UnitsPicker'
+import {colors} from './utils/index'
 
 const WEATHER_API_KEY = 'cc594fd2fb58e9565d6c02e75ac79a95'
 const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?'
@@ -12,12 +14,15 @@ export default function App() {
 
   const [errorMessage, setErrorMessage] = useState(null)
   const [currentWeather, setCurrentWeather] = useState(null)
-  const [unitSystem, setUnitSystem] = useState('metric')
+  const [unitsSystem, setUnitsSystem] = useState('metric')
 
   useEffect(() => {
     load()
-  }, [])
+  }, [unitsSystem])
+
   async function load() {
+    setCurrentWeather(null)
+    setErrorMessage(null)
     try {
 
       let { status } = await Location.requestPermissionsAsync()
@@ -28,9 +33,9 @@ export default function App() {
       }
       const location = await Location.getCurrentPositionAsync()
 
-      const {latitude, longitude} = location.coords
+      const { latitude, longitude } = location.coords
 
-      const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitSystem}&appid=${WEATHER_API_KEY}`
+      const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}`
 
       const response = await fetch(weatherUrl)
 
@@ -49,24 +54,33 @@ export default function App() {
   if(currentWeather) {
     return (
       <View style={styles.container}>
-        <WeatherInfo currentWeather={currentWeather} />
-        <StatusBar style="auto" />
-      </View>
-    );
-  } else {
-    return (
-      <View style={styles.container}>
         <StatusBar style="auto" />
         <View style={styles.main}>
-          <Text>{errorMessage}</Text>
-
+          <UnitsPicker
+          unitsSystem={unitsSystem}
+          setUnitsSystem={setUnitsSystem}
+          />
+         <WeatherInfo currentWeather={currentWeather} />
         </View>
-
       </View>
     );
+  } else if (errorMessage) {
+    return (
+      <View style={styles.container}>
+        <Text>{errorMessage}</Text>
+        <StatusBar style="auto" />
+      </View>
+      );
+    } else {
+        return (
+          <View style={styles.container}>
+            <ActivityIndicator size='large'
+            color={colors.PRIMARY_COLIOR} />
+            <StatusBar style="auto" />
+          </View>
+       );
+    }
   }
-
-}
 
 const styles = StyleSheet.create({
   container: {
---
+++ components/ReloadIcon.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function ReloadIcon() {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
